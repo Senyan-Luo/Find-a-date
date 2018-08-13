4852de4d10ed8711896ca9b9b41c2368192423da
Senyan L <sel152@ucsd.edu>
2018-08-12 18:13:38 -0700

    fixed Ahmed photo

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var surveyData = [
 
   {
     name:"Ahmed",
-    imageURL:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    imageURL:"https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426",
     scores:[
         5,
         1,
